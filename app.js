3c136c380d92d268e20a9161b05d4d7042b20c95
mahdieh <madidgn82@gmail.com>
2024-04-25 21:04:52 +0330

    create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const userInput = document.querySelector("input");
+const form = document.querySelector("form");
+const list = document.querySelector("ul");
+const btnClear = document.querySelector(".clear");
